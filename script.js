d0e51bc256f69f35a7cf6d1375eecfaf9b202538
Jônatas Emanuel Batista <jonatasemanuelbatista12@gmail.com>
2026-01-31 12:13:06 -0300

    Alterações: LocalStorage, Objeto User, Perfil
    localStorage ajustado e funcionando, objeto sendo atualizado e carregado corretamente. Tudo funcionando do load à lista. Comecei a exibir os dados do usuário no perfil com JavaScript

--- script.js
+++ script.js
@@ -7,94 +7,326 @@ let user = {
   tarefasAtivas: [],
 };
 
-// Captura o botão e adiciona evento de clique
-document.getElementById("botaoLogin").addEventListener("click", function () {
-  // Captura o valor do input
-  const nomeValor = document.getElementById("nome").value.trim();
-  const emailValor = document.getElementById("email").value.trim();
-  const senhaValor = document.getElementById("password").value.trim();
+function salvarUsuario() {
+  localStorage.setItem("taskgame_user", JSON.stringify(user));
+}
+function carregarUsuario() {
+  let data = localStorage.getItem("taskgame_user");
+  if (data) {
+    user = JSON.parse(data);
+    return user;
+  }
+  return null;
+}
 
-  // Validação simples
-  if (nomeValor === "") {
-    alert("Por favor, digite um nome.");
-    return;
-  }
-  if (emailValor === "") {
-    alert("Por favor, digite um email.");
-    return;
-  }
-  if (senhaValor === "") {
-    alert("Por favor, digite uma senha.");
-    return;
+document.addEventListener("DOMContentLoaded", () => {
+  const page = document.body.dataset.page;
+
+  if (page === "login") {
+    initLogin();
   }
 
-  // Salva no objeto
-  user.nome = nomeValor;
-  user.email = emailValor;
-  user.senha = senhaValor;
+  if (page === "lista") {
+    initLista();
+  }
 
-  // salvarUsuario(); Será usado para salvar no localStorage
-
-  // Mostra no console para conferência
-  console.log("Objeto atualizado:", user);
-
-  // Muda a página com atraso após salvar o usuário
-  setTimeout(function () {
-    window.location.href = "./apresentacao1.html";
-  }, 3000);
+  if (page === "home") {
+    initHome();
+  }
 });
 
-// LISTA
-const tarefasBase = [
-// Fácil
-  { id: 1, nome: "Escovar os dentes", xp: 20, dificuldade: "Fácil", categoria: "saude"},
-  { id: 2, nome: "Tomar banho", xp: 20, dificuldade: "Fácil", categoria: "saude"},
-  { id: 3, nome: "Lavar o rosto", xp: 20, dificuldade: "Fácil", categoria: "saude"},
-  { id: 4, nome: "Beber 1 copo de água", xp: 20, dificuldade: "Fácil", categoria: "saude"},
-  { id: 5, nome: "Arrumar a cama", xp: 20, dificuldade: "Fácil", categoria: "casa"},
-  { id: 6, nome: "Jogar o lixo fora", xp: 20, dificuldade: "Fácil", categoria: "casa"},
-  { id: 7, nome: "Checar agenda do dia", xp: 20, dificuldade: "Fácil", categoria: "organizacao"},
-  { id: 8, nome: "Atualizar lista de tarefas", xp: 20, dificuldade: "Fácil", categoria: "organizacao"},
-  { id: 9, nome: "Alongar por 5 minutos", xp: 20, dificuldade: "Fácil", categoria: "saude"},
-  { id: 10, nome: "Caminhar 10 minutos", xp: 20, dificuldade: "Fácil", categoria: "saude"},
-// Médio
-  { id: 11, nome: "Lavar a louça", xp: 50, dificuldade: "Médio", categoria: "casa"},
-  { id: 12, nome: "Varrer um cômodo", xp: 50, dificuldade: "Médio", categoria: "casa"},
-  { id: 13, nome: "Lavar roupas", xp: 50, dificuldade: "Médio", categoria: "casa"},
-  { id: 14, nome: "Guardar roupas dobradas", xp: 50, dificuldade: "Médio", categoria: "casa"},
-  { id: 15, nome: "Limpar o banheiro", xp: 50, dificuldade: "Médio", categoria: "casa"},
-  { id: 16, nome: "Estudar por 30 minutos", xp: 50, dificuldade: "Médio", categoria: "estudo"},
-  { id: 17, nome: "Ler 10 páginas", xp: 50, dificuldade: "Médio", categoria: "estudo"},
-  { id: 18, nome: "Caminhar por 30 minutos", xp: 50, dificuldade: "Médio", categoria: "saude"},
-  { id: 19, nome: "Treinar em casa", xp: 50, dificuldade: "Médio", categoria: "saude"},
-  { id: 20, nome: "Cuidar da postura por 1 hora", xp: 50, dificuldade: "Médio", categoria: "saude"},
-  { id: 21, nome: "Limpar e-mails antigos", xp: 50, dificuldade: "Médio", categoria: "organizacao"},
-  { id: 22, nome: "Fazer backup do celular", xp: 50, dificuldade: "Médio", categoria: "organizacao"},
-  { id: 23, nome: "Organizar as pastas no PC", xp: 50, dificuldade: "Médio", categoria: "organizacao"},
-  { id: 24, nome: "Reduzir uso de redes por 1 hora", xp: 50, dificuldade: "Médio", categoria: "saude"},
-// Difícil
-  { id: 25, nome: "Limpar a casa inteira", xp: 80, dificuldade: "Difícil", categoria: "casa"},
-  { id: 26, nome: "Organizar guarda-roupa", xp: 80, dificuldade: "Difícil", categoria: "casa"},
-  { id: 27, nome: "Fazer compra do mês", xp: 80, dificuldade: "Difícil", categoria: "casa"},
-  { id: 28, nome: "Preparar refeições do dia", xp: 80, dificuldade: "Difícil", categoria: "casa"},
-  { id: 29, nome: "Estudar 2 horas focado", xp: 80, dificuldade: "Difícil", categoria: "estudo"},
-  { id: 30, nome: "Revisar estudo do dia", xp: 80, dificuldade: "Difícil", categoria: "estudo"},
-  { id: 31, nome: "Caminhar 1 hora", xp: 80, dificuldade: "Difícil", categoria: "saude"},
-  { id: 32, nome: "Ficar 24 horas sem redes sociais", xp: 80, dificuldade: "Difícil", categoria: "saude"},
-  { id: 33, nome: "Organizar finanças do mês", xp: 80, dificuldade: "Difícil", categoria: "organizacao"},
-  { id: 34, nome: "Organizar agenda inteira", xp: 80, dificuldade: "Difícil", categoria: "organizacao"},
-]
+function initLogin() {
+  // Captura o botão e adiciona evento de clique
+  document.getElementById("botaoLogin").addEventListener("click", function () {
+    
+    function getInputs() {
+      // Captura o valor do input
+      let nomeValor = document.getElementById("nome").value.trim();
+      let emailValor = document.getElementById("email").value.trim();
+      let senhaValor = document.getElementById("password").value.trim();
+      
+      // Validação simples
+      if (!nomeValor || !emailValor || !senhaValor) {
+      alert("Preencha todos os campos.");
+      return;
+    }
 
-const listaEl = document.getElementById("taskList");
+    // Salva no objeto
+      user.nome = nomeValor;
+      user.email = emailValor;
+      user.senha = senhaValor;
+    }
 
-function renderizarTarefas(lista) {
-  listaEl.innerHTML = "";
+    getInputs();
 
-  lista.forEach(tarefa => {
-    const item = document.createElement("div");
-    item.classList.add("tarefas");
+    salvarUsuario(user); // Salva no localStorage
 
-    item.innerHTML = `
+    // Mostra no console para conferência
+    console.log("Objeto atualizado:", user);
+
+    // Muda a página com atraso após salvar o usuário
+    setTimeout(function () {
+      window.location.href = "./apresentacao1.html";
+    }, 3000);
+  });
+}
+
+function initLista() {
+  // LISTA
+  const tarefasBase = [
+    // Fácil
+    {
+      id: 1,
+      nome: "Escovar os dentes",
+      xp: 20,
+      dificuldade: "Fácil",
+      categoria: "saude",
+    },
+    {
+      id: 2,
+      nome: "Tomar banho",
+      xp: 20,
+      dificuldade: "Fácil",
+      categoria: "saude",
+    },
+    {
+      id: 3,
+      nome: "Lavar o rosto",
+      xp: 20,
+      dificuldade: "Fácil",
+      categoria: "saude",
+    },
+    {
+      id: 4,
+      nome: "Beber 1 copo de água",
+      xp: 20,
+      dificuldade: "Fácil",
+      categoria: "saude",
+    },
+    {
+      id: 5,
+      nome: "Arrumar a cama",
+      xp: 20,
+      dificuldade: "Fácil",
+      categoria: "casa",
+    },
+    {
+      id: 6,
+      nome: "Jogar o lixo fora",
+      xp: 20,
+      dificuldade: "Fácil",
+      categoria: "casa",
+    },
+    {
+      id: 7,
+      nome: "Checar agenda do dia",
+      xp: 20,
+      dificuldade: "Fácil",
+      categoria: "organizacao",
+    },
+    {
+      id: 8,
+      nome: "Atualizar lista de tarefas",
+      xp: 20,
+      dificuldade: "Fácil",
+      categoria: "organizacao",
+    },
+    {
+      id: 9,
+      nome: "Alongar por 5 minutos",
+      xp: 20,
+      dificuldade: "Fácil",
+      categoria: "saude",
+    },
+    {
+      id: 10,
+      nome: "Caminhar 10 minutos",
+      xp: 20,
+      dificuldade: "Fácil",
+      categoria: "saude",
+    },
+    // Médio
+    {
+      id: 11,
+      nome: "Lavar a louça",
+      xp: 50,
+      dificuldade: "Médio",
+      categoria: "casa",
+    },
+    {
+      id: 12,
+      nome: "Varrer um cômodo",
+      xp: 50,
+      dificuldade: "Médio",
+      categoria: "casa",
+    },
+    {
+      id: 13,
+      nome: "Lavar roupas",
+      xp: 50,
+      dificuldade: "Médio",
+      categoria: "casa",
+    },
+    {
+      id: 14,
+      nome: "Guardar roupas dobradas",
+      xp: 50,
+      dificuldade: "Médio",
+      categoria: "casa",
+    },
+    {
+      id: 15,
+      nome: "Limpar o banheiro",
+      xp: 50,
+      dificuldade: "Médio",
+      categoria: "casa",
+    },
+    {
+      id: 16,
+      nome: "Estudar por 30 minutos",
+      xp: 50,
+      dificuldade: "Médio",
+      categoria: "estudo",
+    },
+    {
+      id: 17,
+      nome: "Ler 10 páginas",
+      xp: 50,
+      dificuldade: "Médio",
+      categoria: "estudo",
+    },
+    {
+      id: 18,
+      nome: "Caminhar por 30 minutos",
+      xp: 50,
+      dificuldade: "Médio",
+      categoria: "saude",
+    },
+    {
+      id: 19,
+      nome: "Treinar em casa",
+      xp: 50,
+      dificuldade: "Médio",
+      categoria: "saude",
+    },
+    {
+      id: 20,
+      nome: "Cuidar da postura por 1h",
+      xp: 50,
+      dificuldade: "Médio",
+      categoria: "saude",
+    },
+    {
+      id: 21,
+      nome: "Limpar e-mails antigos",
+      xp: 50,
+      dificuldade: "Médio",
+      categoria: "organizacao",
+    },
+    {
+      id: 22,
+      nome: "Fazer backup do celular",
+      xp: 50,
+      dificuldade: "Médio",
+      categoria: "organizacao",
+    },
+    {
+      id: 23,
+      nome: "Organizar as pastas no PC",
+      xp: 50,
+      dificuldade: "Médio",
+      categoria: "organizacao",
+    },
+    {
+      id: 24,
+      nome: "1h sem redes sociais",
+      xp: 50,
+      dificuldade: "Médio",
+      categoria: "saude",
+    },
+    // Difícil
+    {
+      id: 25,
+      nome: "Limpar a casa inteira",
+      xp: 80,
+      dificuldade: "Difícil",
+      categoria: "casa",
+    },
+    {
+      id: 26,
+      nome: "Organizar guarda-roupa",
+      xp: 80,
+      dificuldade: "Difícil",
+      categoria: "casa",
+    },
+    {
+      id: 27,
+      nome: "Fazer compra do mês",
+      xp: 80,
+      dificuldade: "Difícil",
+      categoria: "casa",
+    },
+    {
+      id: 28,
+      nome: "Preparar refeições do dia",
+      xp: 80,
+      dificuldade: "Difícil",
+      categoria: "casa",
+    },
+    {
+      id: 29,
+      nome: "Estudar 2 horas focado",
+      xp: 80,
+      dificuldade: "Difícil",
+      categoria: "estudo",
+    },
+    {
+      id: 30,
+      nome: "Revisar estudo do dia",
+      xp: 80,
+      dificuldade: "Difícil",
+      categoria: "estudo",
+    },
+    {
+      id: 31,
+      nome: "Caminhar 1 hora",
+      xp: 80,
+      dificuldade: "Difícil",
+      categoria: "saude",
+    },
+    {
+      id: 32,
+      nome: "24h sem redes sociais",
+      xp: 80,
+      dificuldade: "Difícil",
+      categoria: "saude",
+    },
+    {
+      id: 33,
+      nome: "Organizar finanças do mês",
+      xp: 80,
+      dificuldade: "Difícil",
+      categoria: "organizacao",
+    },
+    {
+      id: 34,
+      nome: "Organizar agenda inteira",
+      xp: 80,
+      dificuldade: "Difícil",
+      categoria: "organizacao",
+    },
+  ];
+
+  const listaEl = document.getElementById("taskList");
+
+  function renderizarTarefas(lista) {
+    listaEl.innerHTML = "";
+
+    lista.forEach((tarefa) => {
+      const item = document.createElement("div");
+      // item.classList.add("tarefas");
+
+      item.innerHTML = `
       <div class="bar">
       <input type="checkbox" data-id="${tarefa.id}">
       <p><span>Tarefa: </span>${tarefa.nome}</p>
@@ -103,42 +335,67 @@ function renderizarTarefas(lista) {
       </div>
       `;
 
-    listaEl.appendChild(item);
+      listaEl.appendChild(item);
+    });
+  }
+  renderizarTarefas(tarefasBase);
+
+
+  // Eventos após clique no botão
+  document.getElementById("botaoLista").addEventListener("click", function () {
+    carregarUsuario();
+
+    // Filtra tarefas selecionadas
+  function getTarefasSelecionadas() {
+    const checkboxes = document.querySelectorAll(
+      '#taskList input[type="checkbox"]:checked',
+    );
+
+    return Array.from(checkboxes).map((cb) => Number(cb.dataset.id));
+  }
+
+    // Salva tarefas selecionadas no objeto
+    function salvarTarefasSelecionadas() {
+      const idsSelecionados = getTarefasSelecionadas();
+
+      if (idsSelecionados.length <= 4) {
+        alert("Selecione pelo menos 5 tarefas");
+        return;
+      }
+
+      user.tarefasAtivas = tarefasBase.filter((tarefa) =>
+        idsSelecionados.includes(tarefa.id),
+      );
+    }
+
+    salvarTarefasSelecionadas();
+    salvarUsuario();
+
+    console.log("Objeto atualizado:", user.tarefasAtivas);
+
+    setTimeout(function () {
+      window.location.href = "./apresentacao2.html";
+    }, 3000);
   });
 }
-renderizarTarefas(tarefasBase);
 
-// function getTarefasSelecionadas() {
-//   const checkboxes = document.querySelectorAll(
-//     '#taskList input[type="checkbox"]:checked'
-//   );
+function initHome() {
+  carregarUsuario(user);
+  console.log(localStorage.taskgame_user)
 
-//   return Array.from(checkboxes).map(cb =>
-//     Number(cb.dataset.id)
-//   );
-// }
-
-// function salvarTarefasSelecionadas() {
-//   const idsSelecionados = getTarefasSelecionadas();
-
-//   if (idsSelecionados.length === 0) {
-//     alert("Selecione pelo menos uma tarefa");
-//     return;
-//   }
-
-//   user.tarefasAtivas = tarefasBase.filter(tarefa =>
-//     idsSelecionados.includes(tarefa.id)
-//   );
-
-//   salvarUsuario(user);
-//   window.location.href = "apresentacao2.html";
-// }
-
-// function atualizarPerfil(user) {
-//   document.getElementById("userName").textContent = user.nome;
-//   document.getElementById("nivel").textContent = user.nivel;
-//   document.getElementById("score").textContent = user.xp;
-// }
-
-// user = carregarUsuario();
-// if (user) atualizarPerfil(user);
+  const profile = document.getElementById("profile");
+  profile.innerHTML = `<div class="perfil">
+      <h1 id="userName">${user.nome}</h1>
+      <h2 id="nivel">Nível ${user.nivel}</h2>
+      <i class="fa-solid fa-user"></i>
+    </div>
+    <div class="level">
+      <div class="text">
+        <p id="score">${user.xp}  / 1000</p>
+        <p style="color: #1e293b">XP</p>
+      </div>
+      <div class="progress" id="progressBar">
+        <div class="markup"></div>
+      </div>
+    </div>`
+}
